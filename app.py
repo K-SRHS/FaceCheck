7d1b67b92e08076c8d0e987f98de1d5b6c8dfcd3
K-SRHS <yunho000331@naver.com>
2023-06-05 21:45:12 +0000

    2

--- app.py
+++ app.py
@@ -1,44 +1,49 @@
-from flask import Flask, render_template, Response
 import cv2
+from flask import Flask, render_template, Response
+import sys
+
+sys.path.append('/home/runner/FaceCheck-1/venv/lib/python3.10/site-packages')
 
 app = Flask(__name__)
 
 # Load the pre-trained face detection model
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                     'haarcascade_frontalface_default.xml')
 
 # Load the sticker image
-sticker = cv2.imread('C:/Users/yunho/Desktop/PythonWorkspace/MPJ/sticker.png', cv2.IMREAD_UNCHANGED)
-
+sticker = cv2.imread('C:/Users/yunho/Desktop/PythonWorkspace/MPJ/sticker.png',
+                     cv2.IMREAD_UNCHANGED)
 # Convert the sticker to RGBA format
 sticker = cv2.cvtColor(sticker, cv2.COLOR_BGR2BGRA)
-
-# Start capturing video from the default camera
-cap = cv2.VideoCapture(0)
 
 # Initialize flags for selected effects
 apply_mosaic = False
 apply_sticker = False
+
 
 def apply_effects(frame):
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(gray,
+                                          scaleFactor=1.1,
+                                          minNeighbors=5,
+                                          minSize=(30, 30))
 
     # Apply the selected effects to each detected face
     for (x, y, w, h) in faces:
         # Apply mosaic effect
         if apply_mosaic:
             # Extract the region of interest (face) from the frame
-            face = frame[y:y+h, x:x+w]
+            face = frame[y:y + h, x:x + w]
 
             # Apply mosaic blur to the face
             face = cv2.resize(face, (w // 10, h // 10))
             face = cv2.resize(face, (w, h), interpolation=cv2.INTER_NEAREST)
 
             # Replace the original face with the blurred face
-            frame[y:y+h, x:x+w] = face
+            frame[y:y + h, x:x + w] = face
 
         # Apply sticker effect
         if apply_sticker:
@@ -50,16 +55,25 @@ def apply_effects(frame):
             y_offset = y
 
             # Get the region of interest (ROI) in the frame
-            roi = frame[y_offset:y_offset + sticker_resized.shape[0], x_offset:x_offset + sticker_resized.shape[1]]
+            roi = frame[y_offset:y_offset + sticker_resized.shape[0],
+                        x_offset:x_offset + sticker_resized.shape[1]]
 
             # Create a mask to exclude the transparent part of the sticker
             mask = sticker_resized[:, :, 3] / 255.0
 
             # Apply the sticker to the ROI
             for c in range(0, 3):
-                roi[:, :, c] = sticker_resized[:, :, c] * mask + roi[:, :, c] * (1 - mask)
+                roi[:, :,
+                    c] = sticker_resized[:, :, c] * mask + roi[:, :,
+                                                               c] * (1 - mask)
 
     return frame
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 def generate_frames():
     while True:
@@ -72,23 +86,28 @@ def generate_frames():
         # Apply the selected effects to the frame
         output_frame = apply_effects(frame)
 
-        # Convert the frame to JPEG format
+        # Encode the frame as JPEG
         ret, buffer = cv2.imencode('.jpg', output_frame)
 
-        # Get the bytes of the image
+        # Convert the buffer to bytes
         frame_bytes = buffer.tobytes()
 
-        # Yield the frame as an HTTP response
+        # Yield the frame bytes as a response
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    cap = cv2.VideoCapture(0)
+
+    # Check if the webcam is opened successfully
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+
+    app.run(host='0.0.0.0', port=8080, debug=True)
